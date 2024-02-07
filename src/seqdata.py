37bdec19b04dd7b439b18ef48e5013f6ba8d7a34
Fred Jaya <36436914+fredjaya@users.noreply.github.com>
2024-02-07 15:38:32 +1100

    SeqDataView returns self in a new instance, and when sliced

--- src/seqdata.py
+++ src/seqdata.py
@@ -1,9 +1,9 @@
-from dataclasses import dataclass, InitVar, field
-from numpy import array
-from cogent3 import get_moltype
 import typing
+from dataclasses import InitVar, dataclass, field
 
-# import SeqView, override value method
+from cogent3 import get_moltype
+from cogent3.core.sequence import SeqView
+
 
 @dataclass(slots=True)
 class SeqData:
@@ -19,7 +19,9 @@ class SeqData:
         self._name_order = name_order or tuple(data)
         self._moltype = get_moltype(moltype)
 
-    def get_seq_str(self, *, name: str, start: int = None, end: int = None) -> tuple[str]:
+    def get_seq_str(
+        self, *, name: str, start: int = None, end: int = None
+    ) -> tuple[str]:
         return self._data[name][start:end]
 
     def iter_seqs_str(self, *, name_order: list[str] = None) -> typing.Iterator:
@@ -28,3 +30,17 @@ class SeqData:
 
     def iter_names(self, *, name_order: list[str] = None) -> typing.Iterator:
         yield from iter(name_order or self._name_order)
+
+    def iter_seqview_seqs_str(self, *, name_order: list[str] = None) -> typing.Iterator:
+        name_order = name_order or self._name_order
+        yield from (str(SeqView(seq=self._data[n])) for n in name_order)
+
+
+@dataclass
+class SeqDataView(SeqView):
+    def __getitem__(self, index):
+        # super().__getitem__(segment)
+        return self.__repr__()
+
+    def __repr__(self):
+        return self.__class__()
